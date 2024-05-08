373cd6c9edd382ae01165f45811386a6dfb7897b
MikhailChuza <148324632+MikhailChuza@users.noreply.github.com>
2024-05-08 10:58:17 +0300

    Another

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notification = () => {
+  return (
+    <div className="feedbackAlternativeContainer">
+      <p className="feedbackAlternativeText">No feedback yet</p>
+    </div>
+  );
+};
+
+export default Notification;
