d592a7af587a4129d3c7380e89f7b6a20ca56f04
MikhailChuza <148324632+MikhailChuza@users.noreply.github.com>
2024-05-08 10:11:10 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import Description from "./Description/Description";
-import Feedback from "./Feedback/Feedback";
-import Options from "./Options/Options";
-import Notification from "./Notification/Notification";
+// import { useState } from "react";
+// import { useEffect } from "react";
+// import Description from "./Description/Description";
+// import Feedback from "./Feedback/Feedback";
+// import Options from "./Options/Options";
+// import Notification from "./Notification/Notification";
 
-const App = () => {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
-};
+// const App = () => {
+//   const [feedback, setFeedback] = useState({
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   });
+// };
 
-export default App;
+// export default App;
